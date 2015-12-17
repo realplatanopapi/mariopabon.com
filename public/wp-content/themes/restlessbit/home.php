56f95651087abdf097bed1321aadf763a853df0d
Mario Pabon <hey@mariopabon.com>
2015-12-17 16:37:51 -0500

    Show featured posts on home page

--- public/wp-content/themes/restlessbit/home.php
+++ public/wp-content/themes/restlessbit/home.php
@@ -1,5 +1,17 @@
 <?php
 $context = Timber::get_context();
 $post = new TimberPost();
+
 $context['post'] = $post;
+
+$context['featured_posts'] = Timber::get_posts([
+  'meta_query' => [
+    [
+      'key' => 'post_featured',
+      'value' => '"true"',
+      'compare' => 'LIKE'
+    ]
+  ]
+]);
+
 Timber::render( array( 'home.twig' ), $context );
