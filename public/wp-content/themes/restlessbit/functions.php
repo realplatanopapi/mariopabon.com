8210ef5621a626a32d318bc6f4459c1a06b13a59
Mario Pabon <hey@mariopabon.com>
2015-12-17 20:12:46 -0500

    Clean up WordPress cruft

--- public/wp-content/themes/restlessbit/functions.php
+++ public/wp-content/themes/restlessbit/functions.php
@@ -47,3 +47,35 @@ class StarterSite extends TimberSite {
 }
 
 new StarterSite();
+
+function clean_up_head() {
+  // remove junk from head
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wp_generator'); //removes WP Version # for security
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'index_rel_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+  remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+  // all actions related to emojis
+   remove_action( 'admin_print_styles', 'print_emoji_styles' );
+   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+   remove_action( 'wp_print_styles', 'print_emoji_styles' );
+   remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+   // filter to remove TinyMCE emojis
+   add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+
+   // embed script
+   wp_deregister_script( 'wp-embed.min.js' );
+   wp_deregister_script( 'wp-embed' );
+   wp_deregister_script( 'embed' );
+}
+add_action( 'init', 'clean_up_head' );
