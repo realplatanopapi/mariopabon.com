af4157a11a399a25d780d306e3a930edce6ff6f8
Mario Pabon <hey@mariopabon.com>
2015-12-21 20:05:15 -0500

    Only show admin bar to admins

--- public/wp-content/themes/restlessbit/functions.php
+++ public/wp-content/themes/restlessbit/functions.php
@@ -79,3 +79,10 @@ function clean_up_head() {
    wp_deregister_script( 'embed' );
 }
 add_action( 'init', 'clean_up_head' );
+
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
+add_action('after_setup_theme', 'remove_admin_bar');
