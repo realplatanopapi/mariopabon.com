16dfbb633906a616c74a4822667d83c0ef8b5bdc
Mario Pabon <mario@mariopabon.com>
2015-12-26 11:18:41 -0500

    Use local versions of bower and gulp

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 composer update
-npm install -g bower
 cd public/wp-content/themes/restlessbit
-bower install
 npm install
-gulp build:prod
+./node_modules/.bin/bower install
+./node_modules/.bin/gulp build:prod
 rm -rf node_modules
 rm -rf bower_components
