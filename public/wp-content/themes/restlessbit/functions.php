4a6884b51987764e91b7e756059b6191be0f2eee
Mario Pabon <mario@mariopabon.com>
2016-01-29 21:05:54 -0500

    Highlight current page in global nav

--- public/wp-content/themes/restlessbit/functions.php
+++ public/wp-content/themes/restlessbit/functions.php
@@ -31,10 +31,8 @@ class StarterSite extends TimberSite {
 	}
 
 	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
+		$context['current_uri'] = $_SERVER[REQUEST_URI];
 		$context['site'] = $this;
 		return $context;
 	}
