2d387301be261b99f5cd79ec92437a5bf9876124
Mario Pabon <mario@mariopabon.com>
2016-01-29 20:18:48 -0500

    Create home page template

---
+++ public/wp-content/themes/restlessbit/page-home.php
@@ -0,0 +1,19 @@
+<?php
+/** Template Name: Home */
+
+$context = Timber::get_context();
+
+$context['post'] = new TimberPost();
+$context['projects'] = Timber::get_posts( array( 'post_type' => 'project' ) );
+$context['latest_posts'] = Timber::get_posts( array( 'post_type' => 'post', 'posts_per_page' => 5, 'ignore_sticky_posts' => true ) );
+$context['links'] = array(
+  'github' => 'https://github.com/restlessbit',
+  'medium' => 'https://medium.com/@restlessbit',
+  'stackoverflow' => 'http://stackoverflow.com/users/4645524/restlessbit',
+  'twitter' => 'https://twitter.com/restlessbit'
+);
+$context['contact'] = array(
+  'email' => 'mario@mariopabon.com'
+);
+
+Timber::render( array( 'page-home.twig'), $context );
