c9124c4397bc0c0577da75e5ff2245cbdb2cfdc3
Mario Pabon <mario@mariopabon.com>
2016-01-30 14:45:18 -0500

    Remove first page template

--- public/wp-content/themes/restlessbit/index.php
+++ public/wp-content/themes/restlessbit/index.php
@@ -5,18 +5,4 @@ $post = new TimberPost();
 $context['post'] = $post;
 $context['pagination'] = Timber::get_pagination();
 
-if($context['pagination']['current'] == 1) {
-  $context['featured_posts'] = Timber::get_posts([
-    'post__in' => get_option('sticky_posts'),
-    'caller_get_posts' => 1
-  ]);
-
-  $context['latest_posts'] = Timber::get_posts([
-    'ignore_sticky_posts' => true
-  ]);
-
-  Timber::render( array( 'first-page.twig' ), $context );
-}
-else {
-  Timber::render( array( 'index.twig' ), $context);
-}
+Timber::render( array( 'index.twig' ), $context);
