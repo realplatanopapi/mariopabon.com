1920317cb2167536fbcfe962e633c8d0b4ca11bb
Mario Pabon <me@mariopabon.com>
2016-04-23 17:10:00 -0400

    Use domain name instead of ip address in deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ echo "Build successful. Starting deploy..."
 git init
 git add .
 git commit -m "Deploy"
-git remote add production dokku@107.170.7.142:mariopabon.com
+git remote add production dokku@mariopabon.com:mariopabon.com
 git push -f production master
 echo "Deploy successful 🎉!"
