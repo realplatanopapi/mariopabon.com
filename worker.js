a373776fdd9f213b32829d40b821d18fe46f2bde
Mario Pabon <me@mariopabon.com>
2016-07-23 18:00:41 -0400

    Add comments to worker

--- worker.js
+++ worker.js
@@ -1,15 +1,19 @@
 var version = 'v3';
 
+// Invoked on initial installation of the service worker
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(version).then(function(cache) {
+      // Cache all the things
       return cache.matchAll('/');
     })
   );
 });
 
+// Listen for fetch() requests
 self.addEventListener('fetch', function(event) {
   event.respondWith(
+    // Respond with a cached response if there was a network error 
     fetch(event.request).catch(function() {
       return caches.match(event.request);
     })
