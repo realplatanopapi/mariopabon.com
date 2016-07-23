4ef85514df60a36ba903cd38f9e6200793d2bf38
Mario Pabon <me@mariopabon.com>
2016-07-23 17:47:13 -0400

    Fetch from cache if offline

--- worker.js
+++ worker.js
@@ -1,12 +1,22 @@
-this.addEventListener('install', function(event) {
+var version = 'v2';
+
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open(version).then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
         '/css/main.css',
         '/assets/main.js'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
