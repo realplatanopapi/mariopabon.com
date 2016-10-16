fe0368760a671b3202bd5386aa85ac54e10f4a62
Mario Pabon <me@mariopabon.com>
2016-10-15 22:56:26 -0400

    Increase rate of color change

--- scripts/animations/rainbow.js
+++ scripts/animations/rainbow.js
@@ -38,7 +38,7 @@ function animateElementColor ({
 
       return setTimeout(function () {
         requestAnimationFrame(animate)
-      }, 100)
+      }, 75)
     }
 
     // Call onComplete just in case we end up here
