39349532cdda0eef11a3477e679628e791514dd1
Mario Pabon <me@mariopabon.com>
2016-10-27 19:08:37 -0400

    Add code highlighting with prismjs

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,8 @@
+import 'prismjs/prism'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.min'
+
 import bit from './animations/bit'
 import rainbow from './animations/rainbow'
 import smoothScroll from './animations/smooth-scroll'
